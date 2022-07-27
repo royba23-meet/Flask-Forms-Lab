d0a98ef53fba66efaedfe843ed61f3ec2132757e
royba23-meet <roybacharschool@gmail.com>
2022-07-27 13:14:21 +0300

    Finished lab; could be better but good ig

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, jsonify, request, render_template, redirect, url_for
 import random
 
@@ -8,15 +9,35 @@ app = Flask(  # Create a flask app
 )
 
 
-username = "llo2ay"
-password = "123"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+username = "roy"
+password = "yes"
+facebook_friends=["loaf","bread","monkey see", "monkey doo", "monkey stare", "monkey eat"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/', methods=['GET', 'POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		if username == "roy" and password == "yes":
+			return redirect(url_for('home'))
+		else:
+			return render_template('login.html', error="Invalid username or password")
+	else:
+		return render_template('login.html')
+
+@app.route('/home')
+def home():
+	return render_template(
+		'home.html',
+		friends = facebook_friends)
+
+@app.route('/friend_exists/<string:friend>', methods=['GET', 'POST'])
+def friend_exists(friend):
+	return render_template(
+		'friend_exists.html',
+		friend=friend,
+		isFriend = friend in facebook_friends)
 
 
 
